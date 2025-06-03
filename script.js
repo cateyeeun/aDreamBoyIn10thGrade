8ab8013cf45962847aebc122b0c7e056f0a1aa83
Rahma <rahmadina49141@gmail.com>
2025-06-03 21:16:18 +0700

    back btn

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const nextBtn = document.getElementById("nextBtn");
+const backBtn = document.getElementById("backBtn");
 const section1 = document.querySelector(".section-1");
 const section2 = document.querySelector(".section-2");
 
 nextBtn.addEventListener("click", () => {
   section1.classList.add("hidden");
   section2.classList.remove("hidden");
+});
+
+backBtn.addEventListener("click", () => {
+  section2.classList.add("hidden");
+  section1.classList.remove("hidden");
 });
